48cac092aeca84bb2416fbe892d5c89f829130bf
Felipe <felipe.pinto.2912@gmail.com>
2022-08-04 17:35:57 -0300

    Started submit route with required fields validation

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from email import message
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -6,6 +7,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/submit', methods = ['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form["customer"]
+        dealer = request.form["dealer"]
+        rating = request.form["rating"]
+        comments = request.form["comments"]
+        #print(customer, dealer, rating, comments)
+        if customer == '' or dealer == '':
+            return render_template('index.html', message='Please enter required fields!')
+        return render_template('success.html')
+        
+
+    return
 
 if __name__ == '__main__':
     app.debug = True
